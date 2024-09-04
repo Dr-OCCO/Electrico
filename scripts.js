48602c118965a596e2424c7abc42e05ec04eb690
Tatenda Musiiwa <136474718+Dr-OCCO@users.noreply.github.com>
2024-09-04 03:05:15 +0200

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Initialize AOS (Animate On Scroll) library
+AOS.init({
+    duration: 1000, // Duration of animations
+    easing: 'ease-in-out', // Easing function
+    once: true, // Animation should only happen once
+});
